5664994f09c14d85c1e28f15db306f3829551af0
Simon McCartney <simon@mccartney.ie>
2012-06-19 11:01:17 -0400

    Added system variables to the mysql facts

---
+++ lib/facter/mysql.rb
@@ -0,0 +1,96 @@
+# mysql.rb
+# Facts related to MySQL status
+#
+# Author: Frederic -lefred- Descamps
+# <lefred.descamps@gmail.com>
+# http://www.lefred.be/
+# license: GPLv2
+
+# this code is hosted on github : https://github.com/lefred/facter-mysql
+
+# this file should be installed in your facter directory, for example on a fedora 14 it is
+# in /usr/lib/ruby/site_ruby/1.8/facter
+ 
+# mysql command line to execute queries
+mysqlcmd = 'mysql -B -N -e'
+#status = %x[#{mysqlcmd} "SHOW STATUS"].to_s.strip
+status = %x[#{mysqlcmd} "SHOW STATUS"].split("\n")
+replica = %x[#{mysqlcmd} "SHOW SLAVE STATUS\\G"].split("\n")
+
+
+Facter.add('mysql_version') do
+  setcode do
+    isinstalled = false
+    os = Facter.value('operatingsystem')
+    case os
+      when "RedHat", "CentOS", "OEL", "SuSE", "Fedora"
+        [ 'mysql-server', 'MySQL-server-percona', 'Percona-Server-server' ].each { |dbtype|
+          if isinstalled then next
+	  
+          rpm_results  = `rpm -qa #{dbtype}`
+	  isinstalled = !rpm_results.empty?
+          end
+        }
+      when "Debian", "Ubuntu"
+        ['mysql-server', 'Percona-server'].each { |dbtype|
+          if isinstalled then next
+          isinstalled = system "dpkg -l #{dbtype} 2>&1 | egrep '(^ii|^hi)' >/dev/null"
+          end
+        }
+      else
+    end
+    if isinstalled then
+      %x[#{mysqlcmd} "SELECT VERSION()"].to_s.strip
+    end
+  end
+end
+
+mysqlversion = Facter.value('mysql_version')
+
+#
+# mysql_version is failing on my platform (OEL), 
+# so carry on with the fact collection
+if mysqlversion then
+    Facter.add(:mysql_version_server) do
+        setcode do
+            %x[#{mysqlcmd} "SELECT @@VERSION_COMMENT"].to_s.strip
+        end
+    end
+end
+
+    status.each do|n|
+      el=n.split("\t")
+      Facter.add("mysql_#{el[0]}") do
+        setcode do
+            el[1]
+        end
+      end
+    end
+    replica.each do|n|
+      el=n.split(":")
+      Facter.add("mysql_replica_#{el[0].to_s.strip}") do
+        setcode do
+            el[1]
+        end
+      end
+    end
+
+# gather all the global variables
+variables_output = %x[mysqladmin variables].split("\n")
+# get the key/value out of the output & store them in variables
+variables = Hash.new
+variables_output.each { |line| 
+  parsed = /^\|\s(\w+)\s+\|\s+(\w+)/.match(line)
+  if !parsed.nil?
+    key,value = parsed.captures
+    variables[key] = value
+  end
+}
+
+variables.each { |key, value|
+  Facter.add("mysql_#{key}") do
+    setcode do
+      value
+    end
+  end
+} 
